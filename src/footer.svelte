47315d96286cf0ebdc90d921f0bb71ae73311167
Eneakao <info@untitled.es>
2021-05-03 17:06:42 +0200

    actualizada la etiqueta alt de la imagen del footer

--- src/footer.svelte
+++ src/footer.svelte
@@ -1,7 +1,7 @@
 <center>
 	<small>
 		<a href="https://ergoplatform.org">
-			<img src="Ergo-miniatura.png" title="Ergoplatform.org" width="25" class="my-2">
+			<img src="Ergo-miniatura.png" title="Ergoplatform.org" width="25" class="my-2" alt="Logotype Ergo">
 		</a>
 	</small>
 </center>
