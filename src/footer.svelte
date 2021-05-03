d8daa67e19abf1e60ed6c1505ce3c9ae414f9a47
Eneakao <info@untitled.es>
2021-05-03 17:18:15 +0200

    enlace de Github agregado en el footer

--- src/footer.svelte
+++ src/footer.svelte
@@ -3,5 +3,6 @@
 		<a href="https://ergoplatform.org">
 			<img src="Ergo-miniatura.png" title="Ergoplatform.org" width="25" class="my-2" alt="Logotype Ergo">
 		</a>
+		<a href="https://github.com/eneakao/ergotokens" title="Ergotokens.org in Github" class="text-secondary">Github</a>
 	</small>
 </center>
